9791a20bf52dcff214c50f714d53ce250642fc63
Yaroslav <mx.yarik@gmail.com>
2019-03-16 14:24:01 +0300

    refactor

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
-let letter = '<ul class="letter__line"><li class="check">' +
-    '<label><input class="check__input" type="checkbox" onclick="selectLetter(this)">' +
+let defaultLetter = '<ul class="letter__line"><li class="check">' +
+    '<label><input class="check__input" type="checkbox">' +
     '<span class="check__box"></span></label></li>\n' +
     '<li class="author"><img class="author__logo" src="images/logo-author-yandex.png" alt="Я"></li>\n' +
-    '<li class="author-name letter__line_not-read ">Яндекс Мемер</li>\n' +
+    '<li class="author-name letter__line_not-read ">Яндекс Мемер №</li>\n' +
     '<li class="read-mark read-mark_not-read"></li>\n' +
     '<li class="topic letter__line_not-read">Свежий мем из Яндекса! Успей орнуть первым.</li>\n' +
     '<li class="date">16 фев</li></ul>' +
@@ -11,7 +11,7 @@ let letter = '<ul class="letter__line"><li class="check">' +
 let memerId = 0;
 
 function selectAll() {
-    let checkboxes = document.getElementsByClassName('check__input');
+    let checkboxes = document.body.querySelectorAll('.check__input');
     let mainCheck = checkboxes[0];
     for (var i = 1; i < checkboxes.length; i++) {
         checkboxes[i].checked = mainCheck.checked;
@@ -19,23 +19,29 @@ function selectAll() {
 }
 
 function _selectMain(checkbox) {
-    let mainCheck = document.getElementsByClassName('check__input')[0];
+    let mainCheck = document.body.querySelector('.check__input');
     if (!checkbox.checked) {
         mainCheck.checked = false;
     }
 }
 
-function selectLetter(checkbox) {
-    _selectMain(checkbox);
+function selectLetter(event) {
+    if (event.target.className !== 'check__input') return;
+    _selectMain(event.target);
+}
+
+function _updateLetter(letter) {
+    letter.querySelector('.author-name').textContent += memerId++;
+    letter.querySelector('.date').textContent = '16 фев';
 }
 
 function addLetter() {
     let letters = document.getElementById('letter-box__letters');
     let newLetter = document.createElement('li');
     newLetter.className = 'letter';
-    newLetter.innerHTML = letter;
-    newLetter.getElementsByClassName('author-name')[0].innerHTML = 'Яндекс Мемер №' + memerId++;
-    letters.insertBefore(newLetter, letters.children[0]);
+    newLetter.innerHTML = defaultLetter;
+    _updateLetter(newLetter);
+    letters.insertBefore(newLetter, letters.firstChild);
 
     newLetter.style.height = '0';
     newLetter.style.top = '-42px';
@@ -51,7 +57,7 @@ function addLetter() {
 }
 
 function _doActionWithLetters(action) {
-    let checkboxes = document.getElementsByClassName('check__input');
+    let checkboxes = document.body.querySelectorAll('.check__input');
     for (var i = 1; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             var letter = checkboxes[i];
@@ -88,7 +94,6 @@ function animate(draw, duration, complete) {
 
     requestAnimationFrame(function animate(time) {
         var timePassed = time - start;
-        console.log(time, start);
         if (timePassed > duration) timePassed = duration;
         draw(timePassed);
         if (timePassed < duration) {
@@ -118,3 +123,10 @@ function _markReadLetter(letter) {
 function markReadLetters() {
     _doActionWithLetters(_markReadLetter);
 }
+
+document.getElementById('get-letter').addEventListener("click", addLetter);
+document.getElementById('remove-letter').addEventListener("click", removeLetters);
+document.getElementById('spam-letter').addEventListener("click", removeLetters);
+document.getElementById('mark-read-letter').addEventListener("click", markReadLetters);
+
+document.getElementById('letter-box__letters').addEventListener('click', selectLetter);
