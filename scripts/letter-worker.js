ca3495c4f212555d6f250b93d033caff03a173da
Drozdov Gleb <drozdov.gleb.spb@gmail.com>
2019-03-17 03:36:04 +0300

    add delete animation

--- scripts/letter-worker.js
+++ scripts/letter-worker.js
@@ -1,45 +1,65 @@
-let defaultMessage = '                <input class="message-selector" type="checkbox">\n' +
-    '                <img class="ya-img" src="resources/ya-default.png">\n' +
-    '                <div class="message-sender">Яндекс</div>\n' +
-    '                <span class="unread-circle"></span>\n' +
-    '                <div class="message-theme">Соберите всю почту в ящик</div>\n' +
-    '                <div class="message-date">6 июл</div>';
-
-
-
+var counter = 1;
 let messagesContainer = document.getElementById('messages-container');
+
+function createMessage() {
+    return '                <input class="message-selector" type="checkbox">\n' +
+        '                <img class="ya-img" src="resources/ya-default.png">\n' +
+        '                <div class="message-sender">Яндекс</div>\n' +
+        '                <span class="unread-circle"></span>\n' +
+        '                <div class="message-theme">Сообщение №' + counter++ + '</div>\n' +
+        '                <div class="message-date">6 июл</div>';
+}
 
 function addMessage() {
     let newMessage = document.createElement('div');
     newMessage.className = 'message-container';
-    newMessage.innerHTML = defaultMessage;
+    newMessage.innerHTML = createMessage();
     messagesContainer.insertBefore(newMessage, messagesContainer.firstChild);
-    let animationDuration = 500;
-    let height = 30;
-    let durationPerPixel = animationDuration / height;
 
+    let animationDuration = 1000;
+    let fps = animationDuration / 30;
+    animate(newMessage, fps, animationDuration, false);
+}
+
+function animate(el, fps, animationDuration, isRemove) {
     var startTime = performance.now();
     requestAnimationFrame(function animate(curTime) {
         var timePassed = curTime - startTime;
         if (timePassed > animationDuration) timePassed = animationDuration;
+
         function draw(timePassed) {
-            var shift = (timePassed / durationPerPixel);
-            newMessage.style.height = shift + 'px';
+            var shift = (timePassed / fps);
+            el.style.height = isRemove ? (30 - shift) + 'px' : shift + 'px';
+            el.style.opacity = isRemove ? 1 - shift / 30 : shift / 30;
         }
+
         draw(timePassed);
         if (timePassed < animationDuration) {
             requestAnimationFrame(animate);
         }
     });
-    console.log("Hey");
+    if (isRemove) {
+        el.style.paddingTop = '0px';
+        el.style.paddingBottom = '0px';
+        setTimeout(function () {
+            el.parentNode.removeChild(el);
+        }, animationDuration);
+    }
 }
+
+function removeElement(el) {
+    let animationDuration = 1000;
+    let fps = animationDuration / 30;
+    animate(el, fps, animationDuration, true);
+}
+
 
 function deleteMessages() {
     let checkboxes = document.body.querySelectorAll('.message-selector');
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             let message = checkboxes[i].parentElement;
-            messagesContainer.removeChild(message);
+            removeElement(message);
         }
     }
 }
