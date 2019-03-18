23f7f03b592d2fada26095f4cf72361aa3d88bed
Republicn <bloomashka@mail.ru>
2019-03-18 23:07:27 +0300

    added animation

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ let templateLetter = '<input type="checkbox" class="block-inner__checkbox">\n' +
     '<span class="block-inner__sender block-inner__unread">Команда Яндекс.Почты</span>\n' +
     '<div class="block-inner__msg-mark block-inner__mark-unread"></div>\n' +
     '<span class="block-inner__message block-inner__unread">Just a simple test';
-let time = "</span>\n" +'<time class="block-inner__date-msg" datetime="2019-03-16">16 мар</time>\n';
+let time = "</span>\n" + '<time class="block-inner__date-msg" datetime="2019-03-16">16 мар</time>\n';
 
 
 let testLetter = '<input type="checkbox" class="block-inner__checkbox">' +
@@ -20,18 +20,33 @@ function addLetter() {
     let test = document.querySelector('.block-inner__letters')
     let letters = test.childNodes;
     var newLetter = document.createElement('div');
-    newLetter.className = "block-inner__bottom-borders";
-    newLetter.innerHTML = templateLetter + " " + (++count) +  time;
+    newLetter.classList.add("block-inner__bottom-borders");
+    newLetter.classList.add("msg-added");
+    newLetter.innerHTML = templateLetter + " " + (++count) + time;
     test.insertBefore(newLetter, letters[0]);
+}
+
+function deleteMsgs(toDelete) {
+    for (var i = 0; i < toDelete.length; i++) {
+        toDelete[i].remove();
+    }
 }
 
 function deleteLetter() {
     let checkboxes = document.querySelectorAll('.block-inner__checkbox');
+    var toDelete = [];
     for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
-            checkboxes[i].parentElement.remove();
+            var parent = checkboxes[i].parentElement;
+            toDelete.push(parent);
+            if (Math.random() > 0.5) {
+                parent.classList.add("msg-deleted-right");
+            } else {
+                parent.classList.add("msg-deleted-left");
+            }
         }
     }
+    setTimeout(deleteMsgs, 1000, toDelete);
 }
 
 document.getElementById('actions__button-write').addEventListener("click", addLetter);
