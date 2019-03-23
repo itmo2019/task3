3aba4aa519633f04024a11a38c266de992fc8326
daminovn <ndnodir@gmail.com>
2019-03-23 23:40:46 +0300

    bem refactor

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let newMessageBody = ''+
-'<input type="checkbox" class="message-select">\n'+
-'<img class="author-avatar" width="30px" height="30px">\n'+
-'<span class="author-name">Яндекс.Паспорт</span>\n'+
-'<input type="checkbox" class="mark-as-read-tick">\n'+
-'<span class="message-subject">Доступ к аккаунту восстановлен</span>\n'+
-'<time class="message-receive-time">5 авг</time>';
+'<input type="checkbox" class="message__message-select">\n'+
+'<img class="message__author-avatar" width="30px" height="30px">\n'+
+'<span class="message__author-name">Яндекс.Паспорт</span>\n'+
+'<input type="checkbox" class="message__mark-as-read-tick">\n'+
+'<span class="message__message-subject">Доступ к аккаунту восстановлен</span>\n'+
+'<time class="message__message-receive-time">5 авг</time>';
 
 function createMessage() {
-    let list = document.getElementById('message-list');
+    let list = document.getElementsByClassName('content-messages__message-list')[0];
     let newMessage = document.createElement('li');
-    newMessage.className = 'message unread';
+    newMessage.className = 'message message_unread message-list__message';
     newMessage.innerHTML = newMessageBody;
     list.insertBefore(newMessage, list.children[0]);
 
@@ -26,9 +26,8 @@ function createMessage() {
 }
 
 function deleteSelected() {
-    let checkboxes = document.getElementsByClassName('message-select');
-    let list = document.getElementById('message-list');
-    // NOT WORKING BUT WILL FIX
+    let checkboxes = document.getElementsByClassName('message__message-select');
+    let list = document.getElementsByClassName('content-messages__message-list')[0];
     for (let i = 0; i < checkboxes.length; ++i) {
         if (checkboxes[i].checked) {
             let curMessage = checkboxes[i].parentElement;
@@ -44,6 +43,7 @@ function deleteSelected() {
             }
         }
     }
+    
     for (let i = 0; i < checkboxes.length; ++i) {
         if (checkboxes[i].checked) {
             let curMessage = checkboxes[i].parentElement;
