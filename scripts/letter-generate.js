9dd4723abf46ccf4da760a5d1f31424e516c7bce
glcanvas <duginetsn98@gmail.com>
2019-03-27 23:10:33 +0300

    replace in bem mail

--- scripts/letter-generate.js
+++ scripts/letter-generate.js
@@ -7,12 +7,12 @@ createNewLetter.addEventListener("click", addLetter);
 document.addEventListener("DOMContentLoaded", addLetter);
 
 function addLetter() {
-    let items = mailsPlaceholder.getElementsByClassName("mail");
+    let items = mailsPlaceholder.getElementsByClassName("main__mails-placeholder__list-of-mails__mail");
     let element = createLetter(countLetter);
     if (items.length === 0) {
         mailsPlaceholder.appendChild(element);
     } else {
-        let before = mailsPlaceholder.getElementsByClassName("mail")[0];
+        let before = mailsPlaceholder.getElementsByClassName("main__mails-placeholder__list-of-mails__mail")[0];
         mailsPlaceholder.insertBefore(element, before);
     }
     element.addEventListener('click', deleteMessage(countLetter));
@@ -24,7 +24,8 @@ function addLetter() {
 
 function createLetter(id) {
     let mail = document.createElement("div");
-    mail.className = "mail mail_status_not-read";
+    mail.className = "main__mails-placeholder__list-of-mails__mail " +
+        "main__mails-placeholder__list-of-mails__mail_status_not-read";
     mail.setAttribute("id", "mail-" + id.toString());
     mail.appendChild(createCheckBox());
     mail.appendChild(createLogo());
@@ -36,7 +37,8 @@ function createLetter(id) {
 
 function createCheckBox() {
     let checkBox = document.createElement("div");
-    checkBox.className = "mail__item mail__item-mail__select";
+    checkBox.className = "main__mails-placeholder__list-of-mails__mail__item" +
+        " main__mails-placeholder__list-of-mails__mail__select";
     checkBox.innerHTML =
         '                    <label class=\"mails-checkbox\">\n' +
         '                        <input class=\"mails-checkbox__checkbox\" type=\"checkbox\">\n' +
@@ -47,28 +49,28 @@ function createCheckBox() {
 
 function createLogo() {
     let logo = document.createElement("img");
-    logo.className = "mail__item mail__item-mail__logo";
+    logo.className = "main__mails-placeholder__list-of-mails__mail__item main__mails-placeholder__list-of-mails__mail__logo";
     logo.setAttribute("src", "//yastatic.net/mail/socialavatars/socialavatars/v4/ya-default.svg");
     return logo;
 }
 
 function createTitle(id) {
     let title = document.createElement("div");
-    title.className = "mail__item mail__item-mail__title text_hide-by-size";
+    title.className = "main__mails-placeholder__list-of-mails__mail__item main__mails-placeholder__list-of-mails__mail__title text_hide-by-size";
     title.innerText = "всем привей!" + id.toString();
     return title;
 }
 
 function createMessage() {
     let message = document.createElement("div");
-    message.className = "mail__item mail__item-mail__text-message text_hide-by-size";
+    message.className = "main__mails-placeholder__list-of-mails__mail__item main__mails-placeholder__list-of-mails__mail__text-message text_hide-by-size";
     message.innerText = " как дела???";
     return message;
 }
 
 function createDate() {
     let time = document.createElement("div");
-    time.className = "mail__item mail__item-mail__receive-time text_hide-by-size";
+    time.className = "main__mails-placeholder__list-of-mails__mail__item main__mails-placeholder__list-of-mails__mail__receive-time text_hide-by-size";
     time.innerText = "6 янв";
     return time;
 }
