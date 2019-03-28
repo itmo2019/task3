b8f54d628f1cbb6222c201a5a736bdd5708fcffd
glcanvas <duginetsn98@gmail.com>
2019-03-28 13:11:40 +0300

    add anime

--- scripts/letter-generate.js
+++ scripts/letter-generate.js
@@ -5,6 +5,7 @@ let mailsPlaceholder = document.getElementById("mails-placeholder");
 
 createNewLetter.addEventListener("click", addLetter);
 document.addEventListener("DOMContentLoaded", addLetter);
+mailsPlaceholder.addEventListener('click', deleteMessage);
 
 function addLetter() {
     let items = mailsPlaceholder.getElementsByClassName("mail");
@@ -15,74 +16,43 @@ function addLetter() {
         let before = mailsPlaceholder.getElementsByClassName("mail")[0];
         mailsPlaceholder.insertBefore(element, before);
     }
-    element.addEventListener('click', deleteMessage(countLetter));
-    element.addEventListener('animationend', (event) => {
-        event.target.remove();
-    });
+    setTimeout(() => {
+        element.classList.add("create-animation");
+    }, 0);
     countLetter++;
 }
 
 function createLetter(id) {
-    let mail = document.createElement("div");
-    mail.className = "mail " +
-        "mail_status_not-read";
-    mail.setAttribute("id", "mail-" + id.toString());
-    mail.appendChild(createCheckBox());
-    mail.appendChild(createLogo());
-    mail.appendChild(createTitle(id));
-    mail.appendChild(createMessage());
-    mail.appendChild(createDate());
-    return mail;
+    let template = document.querySelector("#mail-template");
+    let innerDiv = document.importNode(template.content.querySelector("div"), true);
+    let titleDiv = innerDiv.querySelector(".mail__title");
+    innerDiv.setAttribute("id", "mail-" + id.toString());
+    titleDiv.innerText += id.toString();
+    return innerDiv;
 }
 
-function createCheckBox() {
-    let checkBox = document.createElement("div");
-    checkBox.className = "mail__item" +
-        " mail__select";
-    checkBox.innerHTML =
-        '                    <label class=\"mails-checkbox\">\n' +
-        '                        <input class=\"mails-checkbox__checkbox\" type=\"checkbox\">\n' +
-        '                        <span class=\"mails-checkbox__alternative-drawing\"></span>\n' +
-        '                    </label>\n';
-    return checkBox;
+function deleteMessage(event) {
+    if (event.target === undefined || event.target.getAttribute("data-delete") === null) {
+        return;
+    }
+    let mail = findMail(event.target);
+    if (mail === null) {
+        return;
+    }
+    mail.classList.add("delete-animation");
+    mail.addEventListener('transitionend', (event) => {
+        event.target.remove();
+    });
 }
 
-function createLogo() {
-    let logo = document.createElement("img");
-    logo.className = "mail__item mail__logo";
-    logo.setAttribute("src", "//yastatic.net/mail/socialavatars/socialavatars/v4/ya-default.svg");
-    return logo;
-}
-
-function createTitle(id) {
-    let title = document.createElement("div");
-    title.className = "mail__item mail__title text_hide-by-size";
-    title.innerText = "всем привей!" + id.toString();
-    return title;
-}
-
-function createMessage() {
-    let message = document.createElement("div");
-    message.className = "mail__item mail__text-message text_hide-by-size";
-    message.innerText = " как дела???";
-    return message;
-}
-
-function createDate() {
-    let time = document.createElement("div");
-    time.className = "mail__item mail__receive-time text_hide-by-size";
-    time.innerText = "6 янв";
-    return time;
-}
-
-function deleteMessage(item) {
-    return () => {
-        let element = document.getElementById("mail-" + item.toString());
-        if (element === null) {
-            console.log("can't delete mail-" + item.toString());
-            return;
+function findMail(element) {
+    let tmp = element;
+    while (tmp !== null) {
+        let classes = tmp.classList;
+        if (classes.contains('mail') && tmp.getAttribute('id') !== null) {
+            return tmp;
         }
-        element.classList.add("delete-animation");
-    };
+        tmp = tmp.parentNode;
+    }
+    return null;
 }
-
