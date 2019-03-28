e677de227db1647411e366a839c588c8be27f3ec
am.volkov <am.volkov@skbkontur.ru>
2019-03-28 22:16:10 +0300

    refactoring

--- js/letters.js
+++ js/letters.js
@@ -1,9 +1,6 @@
-let children = [];
-let ctx = undefined;
-
 function sendLetter() {
     const mails = document.getElementById("letters");
-    const letter = buildLetter();
+    const letter = generateLetter();
 
     let before;
     if (mails.childNodes.length === 0) {
@@ -11,83 +8,57 @@ function sendLetter() {
     } else {
         before = mails.childNodes.values().next().value;
     }
-    setAnimation(letter, "create_animation 0.25s");
-    mails.insertBefore(letter, before)
-}
+    mails.insertBefore(letter, before);
 
+    setTimeout(() => setCreateTransition(letter), 10);
+}
 
 function removeLetters() {
+    const mails = document.getElementById("letters");
+
     const checked = []
         .filter
-        .call(document.getElementsByClassName("ya-checkbox"), (elem) => elem.checked)
-        .filter((elem) => elem.id.localeCompare("select-all") !== 0 )
+        .call(mails.getElementsByClassName("ya-checkbox"), (elem) => elem.checked)
         .map((doc) => doc.parentElement);
 
+    setTimeout(() => markAsRemoved(checked), 10);
+
+    mails.addEventListener("transitionend", () => checked.forEach((elem) => mails.removeChild(elem)))
+}
+
+function generateLetter() {
+    return buildLetter("img/ya-default.svg", "Росреестр", "Ваша выписка готова", "18 мрт");
+}
+
+function buildLetter(avatar, sender, title, date) {
+    let letter = getLetterTemplate();
+
+    setText(letter, ".letters__mail_sender", sender);
+    setText(letter, ".letters__mail_user-avatar", avatar);
+    setText(letter, ".letters__mail_message-title", title);
+    setText(letter, ".letters__mail_receive-time", date);
+
+    return letter;
+}
+
+function getLetterTemplate() {
+    let link = document.querySelector('link[href="template/letter_template.html"]');
+    let content = link.import.cloneNode(true);
+
+    return content.querySelector('.letters_mail');
+}
+
+function setText(letter, className, text) {
+    let field = letter.querySelector(className);
+    field.innerText = text
+}
+
+function setCreateTransition(letter) {
+    return letter.className += " letters_mail_show";
+}
+
+function markAsRemoved(checked) {
     checked.forEach((parent) => {
-        setAnimation(parent, "delete_animation 0.6s");
-    });
-
-    const mails = document.getElementById("letters");
-    setTimeout(() => checked.forEach((elem) => mails.removeChild(elem)), 700)
-}
-
-function buildLetter() {
-    return tg("div", "letters_mail unread", () => {
-        createYaCheckbox();
-        createUserAvatar();
-        createSender();
-        createTitle();
-        createReceiveTime();
-    });
-}
-
-function createTitle() {
-    children.push(tg("input", "letters__mail_read-mark", () => {
-        ctx.setAttribute("type", "checkbox");
-    }));
-    children.push(tg("span", "letters__mail_message-title", () => "Ваша выписка готова"));
-}
-
-function createSender() {
-    children.push(tg("span", "letters__mail_sender", () => "Росреестр"));
-}
-
-function createReceiveTime() {
-    children.push(tg("span", "letters__mail_receive-time", () => "18 мрт"));
-}
-
-function createYaCheckbox() {
-    children.push(tg("input", "ya-checkbox", () => {
-        ctx.setAttribute("type", "checkbox");
-    }));
-}
-
-function createUserAvatar() {
-    children.push(tg("img", "letters__mail_user-avatar", () => {
-        ctx.setAttribute("src", "img/ya-default.svg");
-    }));
-}
-
-function setAnimation(elem, animation) {
-    elem.style.setProperty("animation", animation);
-    elem.style.setProperty("animation-fill-mode", "forwards");
-}
-
-function tg(tag, style, builder) {
-    const element = document.createElement(tag);
-    const oldCtx = ctx;
-    const oldChildren = children;
-    children = [];
-    ctx = element;
-    element.className = style;
-    if (builder !== undefined) {
-        const ret = builder();
-        if (ret !== undefined) {
-            element.innerText = ret
-        }
-    }
-    children.forEach((child) => element.appendChild(child));
-    ctx = oldCtx;
-    children = oldChildren;
-    return element
+        parent.className += " letters_mail_will_be_removed";
+    })
 }
