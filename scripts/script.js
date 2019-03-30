2e146e5c6920a31751e22d41e355150dc4bab68f
Vitaliy <www.vitaliy_14@mail.ru>
2019-03-30 17:16:10 +0300

    task 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,63 @@
-var letter = "<div class=\"letter\">\n" +
-    "    <div class=\"sender-info-container\">\n" +
-    "        <div class=\"sender-info-container__checkbox\"></div>\n" +
-    "        <div class=\"sender-info-container__photo\"></div>\n" +
-    "        <div class=\"sender-info-container__theme\">Task 1</div>\n" +
+const letter =
+    "<div class=\"mail-body__letters-window__body__letter\">\n" +
+    "    <label class=\"check\">\n" +
+    "        <input class=\"check__input\" type=\"checkbox\">\n" +
+    "        <span class=\"check__box\"></span>\n" +
+    "    </label>\n" +
+    "    <div class=\"mail-body__letters-window__body__letter__photo\"></div>\n" +
+    "    <div class=\"mail-body__letters-window__body__letter__author\">Гламозда Виталий</div>\n" +
+    "    <div class=\"mail-body__letters-window__body__letter__readed\"></div>\n" +
+    "    <div class=\"mail-body__letters-window__body__letter__theme\">Здравствуйте, отправил 1 задание</div>\n" +
+    "    <div class=\"mail-body__letters-window__body__letter__data\">\n" +
+    "        <time datetime=\"2019-03-01\">3 мар</time>\n" +
     "    </div>\n" +
-    "    <div class=\"readed\"></div>\n" +
-    "    <div class=\"letter-info-container\">\n" +
-    "        <div class=\"letter-info-container__begin-of-letter\">Здравствуйте, отправил 1 задание</div>\n" +
-    "        <div class=\"letter-info-container__data\">\n" +
-    "            <time datetime=\"2019-03-01\">3 мар</time>\n" +
-    "        </div>\n" +
-    "        <div class=\"trashcan\" onclick=\"removeLetter(event)\"></div>" +
-    "    </div>\n" +
-    "</div>" +
+    "</div>\n" +
     "<div class=\"line\"></div>";
+
+let letterCounter = 0;
 
 function addLetter() {
     let letters = document.getElementById("letters");
     let bodyLetters = letters.children[2];
+    letterCounter++;
     bodyLetters.insertAdjacentHTML("beforeend", letter);
 }
 
-function removeLetter(event) {
+function mainCheckBoxClicked(event) {
     let letters = document.getElementById("letters");
     let bodyLetters = letters.children[2];
-    bodyLetters.removeChild(event.currentTarget.parentNode.parentNode.nextSibling);
-    bodyLetters.removeChild(event.currentTarget.parentNode.parentNode);
+    let inputCheckbox = event.target.previousElementSibling;
+    for (let i = 0; i < bodyLetters.childElementCount; i++) {
+        if (i % 2 === 0) {
+            bodyLetters.children[i].children[0].children[0].checked = !inputCheckbox.checked;
+        }
+    }
 }
 
+function removeCheckedLetters() {
+    let letters = document.getElementById("letters");
+    let bodyLetters = letters.children[2];
+    for (let i = 0; i < bodyLetters.childElementCount; i++) {
+        if (i % 2 === 0) {
+            if (bodyLetters.children[i].children[0].children[0].checked) {
+                bodyLetters.children[i + 1].classList.add('start-transition');
+                bodyLetters.children[i].classList.add('start-transition');
+                bodyLetters.children[i + 1].addEventListener('transitionend', function () {
+                    bodyLetters.removeChild(this);
+                });
+                bodyLetters.children[i].addEventListener('transitionend', function () {
+                    bodyLetters.removeChild(this);
+                });
+                letterCounter--;
+            }
+        }
+    }
 
-
+    if (letterCounter === 0) {
+        console.log("sadas");
+        let mainCheckbox = document.getElementById("mainCheckbox");
+        if (mainCheckbox.checked) {
+            mainCheckbox.checked = false;
+        }
+    }
+}
