39483c89cc70e5c78cf5b79ec232b12e6ebebe29
Republicn <bloomashka@mail.ru>
2019-03-30 21:58:08 +0300

    fix

--- code.js
+++ code.js
@@ -1,27 +1,27 @@
-let templateLetter = '<input type="checkbox" class="block-inner__checkbox">\n' +
-    '<img class="block-inner__pic" alt="logo" src="https://yastatic.net/mail/socialavatars/socialavatars/v4/ya-default.svg">\n' +
-    '<span class="block-inner__sender block-inner__unread">Команда Яндекс.Почты</span>\n' +
-    '<div class="block-inner__msg-mark block-inner__mark-unread"></div>\n' +
-    '<span class="block-inner__message block-inner__unread">Just a simple test';
-let time = "</span>\n" + '<time class="block-inner__date-msg" datetime="2019-03-16">16 мар</time>\n';
+let templateLetter = '<input type="checkbox" class="letter__checkbox">\n' +
+    '<img class="letter__pic" alt="logo" src="https://yastatic.net/mail/socialavatars/socialavatars/v4/ya-default.svg">\n' +
+    '<span class="letter__sender letter__unread">Команда Яндекс.Почты</span>\n' +
+    '<div class="letter__msg-mark letter__mark-unread"></div>\n' +
+    '<span class="letter__message letter__unread">Just a simple test';
+let time = "</span>\n" + '<time class="letter__date-msg" datetime="2019-03-16">16 мар</time>\n';
 
 
-let testLetter = '<input type="checkbox" class="block-inner__checkbox">' +
-    '<img class="block-inner__pic" alt="logo"' +
+let testLetter = '<input type="checkbox" class="letter__checkbox">' +
+    '<img class="letter__pic" alt="logo"' +
     'src="https://yastatic.net/mail/socialavatars/socialavatars/v4/ya-default.svg">' +
-    '<span class="block-inner__sender block-inner__unread">Команда Яндекс.Почты</span>' +
-    '<div class="block-inner__msg-mark block-inner__mark-unread"></div>' +
-    '<span class="block-inner__message block-inner__unread">Как читать почту с' +
+    '<span class="letter__sender letter__unread">Команда Яндекс.Почты</span>' +
+    '<div class="letter__msg-mark letter__mark-unread"></div>' +
+    '<span class="letter__message letter__unread">Как читать почту с' +
     'мобильногоiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</span>' +
-    '<time class="block-inner__date-msg" datetime="2018-07-06">6 июл</time>';
+    '<time class="letter__date-msg" datetime="2018-07-06">6 июл</time>';
 
 let count = 0
 function addLetter() {
     let test = document.querySelector('.block-inner__letters')
     let letters = test.childNodes;
     var newLetter = document.createElement('div');
-    newLetter.classList.add("block-inner__bottom-borders");
-    newLetter.classList.add("msg-added");
+    newLetter.classList.add("letters__letter");
+    newLetter.classList.add("letter");
     newLetter.innerHTML = templateLetter + " " + (++count) + time;
     test.insertBefore(newLetter, letters[0]);
 }
@@ -33,7 +33,7 @@ function deleteMsgs(toDelete) {
 }
 
 function deleteLetter() {
-    let checkboxes = document.querySelectorAll('.block-inner__checkbox');
+    let checkboxes = document.querySelectorAll('.letter__checkbox');
     var toDelete = [];
     for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
@@ -50,4 +50,4 @@ function deleteLetter() {
 }
 
 document.getElementById('actions__button-write').addEventListener("click", addLetter);
-document.getElementById('block-inner__delete').addEventListener("click", deleteLetter);
+document.getElementById('can-do__delete').addEventListener("click", deleteLetter);
