c150d8392b10e47684f6d0f527cafbea5bf3e9f9
Egor Galkin <egor.galkin99@yandex.ru>
2019-04-06 15:49:49 +0300

    Imporve class naming

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const mailHtml = '                <section class="mail _bottom-separator-view">\
     '                            <div class="chose mail__elem mail__elem_view">\n' +
     '                                <input class="checkbox" type="checkbox">\n' +
     '                            </div>\n' +
-    '                            <a href="#popup-essay" class="_simple-link-view">\n' +
+    '                            <a href="#popup-essay" class="simple-link_view">\n' +
     '                                <div class="mail__sender-logo mail__ivanov-logo_view"><p class="ii">И</p></div>\n' +
     '                                <div class="mail__author mail__author_view">\n' +
     '                                    <p class="sender-ivan sender-ivan_view _message_view">Иван Иванов</p>\n' +
     '                                </div>\n' +
     '                                <div class="read-status read-status_view read-status_not-read"></div>\n' +
     '                                <div class="mail__message-body mail__message-body_view">\n' +
-    '                                    <p class="ivan-body ivan-body_view _message_view _text-overflow-behaviour">Съешь же ещё этих мягких французских булок, да выпей чаю</p>\n' +
+    '                                    <p class="ivan-body ivan-body_view _message_view text-overflow_behaviour">Съешь же ещё этих мягких французских булок, да выпей чаю</p>\n' +
     '                                </div>\n' +
     '                                <div class="message-date message-date_view">\n' +
     '                                    <p class="date date_view _message_view">6 июл</p>\n' +
