e21dafb224f0b4cbab5e113fa81529803faae29e
okayvlev <ilyuhaya@gmail.com>
2019-04-16 12:58:45 +0300

    Fixed

--- js/app.js
+++ js/app.js
@@ -9,28 +9,43 @@ document.onkeypress = function(event) {
 
 function newMessage() {
     var currentMessage = document.getElementsByClassName("mailbox__mail")[0];
-    if (currentMessage.getAttribute("state") != "hidden") {
+    if (currentMessage.getAttribute("data-state") != "hidden") {
         return;
     }
-    currentMessage.setAttribute("state", "showing");
-    setTimeout(function() {
-        currentMessage.removeAttribute("state");
-    }, 400);
+    var newMessage = currentMessage.cloneNode(true);
+    document.getElementsByClassName("mailbox__mail-list")[0].insertBefore(newMessage, currentMessage);
+    currentMessage.setAttribute("data-state", "showing");
+    currentMessage.addEventListener('transitionend', function() {
+        currentMessage.removeAttribute("data-state");
+    });
 }
 
 function deleteMessage() {
+    deleteMessageByIndex(3);
+}
+
+function deleteMessageByIndex(index) {
     var messages = document.getElementsByClassName("mailbox__mail");
-    var index = 3;
 
     if (index >= messages.length) {
-        window.alert("No spam messages to delete");
+        window.alert("Cannot delete message");
         return;
     }
 
     var currentMessage = messages[index];
-    currentMessage.setAttribute("state", "deleted");
-    var parent = currentMessage.parentNode;
-    setTimeout(function() {
-        currentMessage.remove();
-    }, 400);
+    deleteMessageByElement(currentMessage);
+}
+
+function deleteMessageByElement(messageElement) {
+    messageElement.setAttribute("data-state", "deleted");
+    
+    messageElement.addEventListener('animationend', function() {
+        messageElement.remove();
+    });
+}
+
+function deleteSelected() {
+    var mailList = Array.from(document.getElementsByClassName("mailbox__mail"));
+    var checkedMailList = mailList.filter(mail => mail.getElementsByClassName("checkbox__input")[0].checked);
+    checkedMailList.forEach(deleteMessageByElement);
 }
